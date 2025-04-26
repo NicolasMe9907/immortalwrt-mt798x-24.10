e8fafbf6c780cc12cbac9a8e2aa3e1c1bb53955e
NicolasMe9907 <mooner.nicolas999@proton.me>
2025-04-27 07:52:44 +0800

    mediatek: 3kme&xr30e.52MHz&Cudy.114M

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -3,8 +3,7 @@ KERNEL_LOADADDR := 0x48080000
 MT7981_USB_PKGS := automount blkid blockdev fdisk \
     kmod-nls-cp437 kmod-nls-iso8859-1 kmod-usb2 kmod-usb3 \
     luci-app-usb-printer luci-i18n-usb-printer-zh-cn \
-    kmod-usb-net-rndis usbutils \
-    kmod-usb-net-qmi-wwan autoksmbd
+    kmod-usb-net-rndis usbutils luci-app-usbmodem
 
 define Device/mt7981-spim-nor-rfb
   DEVICE_VENDOR := MediaTek
@@ -479,7 +478,7 @@ define Device/cudy_tr3000-v1
    UBINIZE_OPTS := -E 5
    BLOCKSIZE := 128k
    PAGESIZE := 2048
-   IMAGE_SIZE := 65536k
+   IMAGE_SIZE := 114688k
    KERNEL_IN_UBI := 1
    IMAGES += factory.bin
    IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
@@ -795,22 +794,3 @@ define Device/e-life_etr635-u
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += e-life_etr635-u
-
-define Device/Airpi-emmc-manper
-  DEVICE_VENDOR := Airpi
-  DEVICE_MODEL := Airpi-emmc-manper
-  DEVICE_DTS := mt7981-Airpi-emmc16G
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := 7981-Airpi
-  UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  IMAGE_SIZE := 131072k
-  KERNEL_IN_UBI := 1
-  IMAGES += factory.bin
-  DEVICE_PACKAGES := $(MT7981_USB_PKGS) f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
-         luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
-  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += Airpi-emmc-manper
