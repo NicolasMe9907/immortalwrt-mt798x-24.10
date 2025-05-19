e047b333743127bcec9bc8943a2ce6ba64011622
padavanonly <padavanonly@github.com>
2025-05-20 00:08:04 +0800

    airpi:adjust driver

--- package/kernel/Airpi-gpio-fan/src/Airpi-gpio-fan.c
+++ package/kernel/Airpi-gpio-fan/src/Airpi-gpio-fan.c
@@ -3,18 +3,53 @@
 #include <linux/gpio.h>
 #include <linux/hrtimer.h>
 #include <linux/ktime.h>
+#include <linux/thermal.h>
 
 #define GPIO_FAN 483
 #define MAX_DUTY_CYCLE 255
 #define PWM_PERIOD_NS 1000000 // 1ms period
 //by Manper 20250519
+static struct thermal_cooling_device *fan_cdev;
 static int duty_cycle = 0; // Duty cycle value from 0 to 255
 static struct hrtimer pwm_timer;
 static ktime_t ktime_period;
+static unsigned int current_state = 0;
+static int fan_get_max_state(struct thermal_cooling_device *cdev, unsigned long *state) {
+    *state = 3;
+    return 0;
+}
+
+static int fan_get_cur_state(struct thermal_cooling_device *cdev, unsigned long *state) {
+    *state = current_state;
+    return 0;
+}
+
+static int fan_set_cur_state(struct thermal_cooling_device *cdev, unsigned long state) {
+    if (state > 3)
+        return -EINVAL;
+
+    current_state = state;
+
+    switch (state) {
+    case 0: duty_cycle = 40; break;
+    case 1: duty_cycle = 80; break;
+    case 2: duty_cycle = 160; break;
+    case 3: duty_cycle = 220; break;
+    }
+
+    return 0;
+}
+
+static const struct thermal_cooling_device_ops fan_cooling_ops = {
+    .get_max_state = fan_get_max_state,
+    .get_cur_state = fan_get_cur_state,
+    .set_cur_state = fan_set_cur_state,
+};
 
 static enum hrtimer_restart pwm_timer_callback(struct hrtimer *timer) {
     static bool fan_on = false;
-    int on_time_ns = (duty_cycle * PWM_PERIOD_NS) / MAX_DUTY_CYCLE;
+    int dc = READ_ONCE(duty_cycle);
+    int on_time_ns = ((MAX_DUTY_CYCLE - dc) * PWM_PERIOD_NS) / MAX_DUTY_CYCLE;
     ktime_t ktime_on = ktime_set(0, on_time_ns);
     ktime_t ktime_off = ktime_set(0, PWM_PERIOD_NS - on_time_ns);
 
@@ -70,7 +105,13 @@ static int __init gpio_pwm_fan_init(void) {
         gpio_free(GPIO_FAN);
         return ret;
     }
-
+    fan_cdev = thermal_cooling_device_register("pwm_fan", NULL, &fan_cooling_ops);
+    if (IS_ERR(fan_cdev)) {
+   	gpio_free(GPIO_FAN);
+    	hrtimer_cancel(&pwm_timer);
+    	sysfs_remove_file(kernel_kobj, &duty_cycle_attr.attr);
+    	return PTR_ERR(fan_cdev);
+    }
     printk(KERN_INFO "GPIO PWM Fan module loaded\n");
     return 0;
 }
@@ -79,6 +120,7 @@ static void __exit gpio_pwm_fan_exit(void) {
     hrtimer_cancel(&pwm_timer);
     sysfs_remove_file(kernel_kobj, &duty_cycle_attr.attr);
     gpio_free(GPIO_FAN);
+    thermal_cooling_device_unregister(fan_cdev);
     printk(KERN_INFO "GPIO PWM Fan module unloaded\n");
 }
 
